dbb1f680031877c868fe2981040818e342e66474
AlvinKuria <alvinkuria18@gmail.com>
2023-07-27 19:50:22 +0100

    Html and CSS finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-cosole.log('Im working');
+const buttons = document.querySelectorAll('[data-carousel-button]');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
+    const slides = button.closest('[data-carousel]').querySelectorAll('.slide');
+  })
+})
