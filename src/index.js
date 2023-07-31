613402f862ac0f77145be297c75c1fe3aefd392e
AlvinKuria <alvinkuria18@gmail.com>
2023-07-31 19:55:19 +0100

    CSS Transitions is done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 const buttons = document.querySelectorAll('[data-slider-button]');
 
+const navigationDots = document.querySelector('.navigation-dots');
+
+function updateNavigationDots() {
+  const slides = document.querySelectorAll('[data-slides] .slide');
+  navigationDots.innerHTML = '';
+
+  slides.forEach((slide, index) => {
+    const dot = document.createElement('span');
+    dot.classList.add('dot');
+    if (slide.dataset.active) {
+      dot.classList.add('active');
+    }
+
+    dot.addEventListener('click', () => {
+      const activeSlide = document.querySelector('[data-slides] [data-active]');
+      if (activeSlide) {
+        activeSlide.dataset.active = false;
+      }
+      slide.dataset.active = true;
+      updateNavigationDots();
+    });
+
+    navigationDots.appendChild(dot);
+  });
+}
+
+window.addEventListener('load', updateNavigationDots());
+
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     const offset = button.dataset.sliderButton === 'next' ? 1 : -1;
@@ -14,5 +42,7 @@ buttons.forEach((button) => {
 
     slides.children[newIndex].dataset.active = true;
     delete activeSlide.dataset.active;
+
+    updateNavigationDots();
   });
 });
