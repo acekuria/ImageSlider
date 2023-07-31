42c42555214aadcdb1ca9c2f1b43c37f855e8183
AlvinKuria <alvinkuria18@gmail.com>
2023-07-31 17:34:36 +0100

    Html and CSS edited

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const buttons = document.querySelectorAll('[data-carousel-button]');
+const buttons = document.querySelectorAll('[data-slider-button]');
 
-buttons.forEach(button => {
+buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
-    const slides = button.closest('[data-carousel]').querySelectorAll('.slide');
-  })
-})
+    const offset = button.dataset.sliderButton === 'next' ? 1 : -1;
+    const slides = button.closest('[data-slider]').querySelectorAll('.slide');
+  });
+});
